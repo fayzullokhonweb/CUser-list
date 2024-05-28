1fc5018538291655703a8a8b21a71ebffced1dc2
Fayzulloxon Rasulov <154775790+fayzullokhonweb@users.noreply.github.com>
2024-05-28 22:22:20 +0500

    add new title

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,15 +1,15 @@
 // style
-import './Footer.css'
+import "./Footer.css";
 
 function Footer() {
   return (
-    <div className='footer'>
+    <div className="footer">
       <div className="footer-container">
         <p>All rights reserved.</p>
-        <a href="https://t.me/anvarov23"> Davronbek Anvarov</a>
+        <a href="https://t.me/anvarov23">Fayzullokhon Rasulkhonov</a>
       </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
